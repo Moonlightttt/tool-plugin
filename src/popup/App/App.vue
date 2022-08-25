2eb4462448a386bbe0c34e3cc60e25e94edd62be
moonlightttt <595241427@qq.com>
2022-08-25 14:56:23 +0800

    新增点击复制

--- src/popup/App/App.vue
+++ src/popup/App/App.vue
@@ -1,35 +1,53 @@
 <template>
-  <div class="main_app">
-    <el-input class="source" v-model="sourceValue" :rows="5" type="textarea" placeholder="Please input" />
-    <el-button class="send" type="primary" @click="send">ChangeToCamel</el-button>
-    <el-input class="destination" v-model="destinationValue" :rows="5" type="textarea" placeholder="Please input" />
+  <div class="main_app" v-loading="loading">
+    <el-input class="source" v-model="sourceValue" :rows="5" type="textarea" placeholder="请输入需要转换的Json" />
+    <el-button class="commonBtn" type="primary" @click="send">ConvertToCamel</el-button>
+    <el-input class="destination" v-model="destinationValue" :rows="5" type="textarea" placeholder="" />
+    <el-button class="commonBtn" type="primary" @click="copy">ClickToCopy</el-button>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 import { convertToCamel } from '@/api'
+import useClipboard from 'vue-clipboard3'
+
+const { toClipboard } = useClipboard()
+
 export default {
   name: 'app',
   data() {
     return {
-      url: "http://localhost:8899/Tool/ConvertToCamel",
       sourceValue: '',
-      destinationValue: ''
+      destinationValue: '',
+      loading: ref(false),
     }
   },
   methods: {
     send() {
+      this.loading = true
       convertToCamel({
         jsonStr: this.sourceValue
       })
         .then(res => {
           console.log(res)
-          this.destinationValue = JSON.stringify(res);
+          this.loading = false
+          if (res) {
+            this.destinationValue = JSON.stringify(res)
+          }
         })
         .catch(err => {
           console.log(err)
         })
     },
+    async copy() {
+      try {
+        await toClipboard(this.destinationValue)
+        console.log('Copied to clipboard')
+      } catch (e) {
+        console.error(e)
+      }
+    }
   },
 }
 </script>
@@ -39,7 +57,7 @@ export default {
   text-align: center;
 }
 
-.send {
+.commonBtn {
   margin: 5px;
 }
 
