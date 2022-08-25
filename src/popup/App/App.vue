a463db443f92d0fe484857d290bc7c61b40aff44
moonlightttt <595241427@qq.com>
2022-08-25 16:28:32 +0800

    提交

--- src/popup/App/App.vue
+++ src/popup/App/App.vue
@@ -1,56 +1,72 @@
 <template>
   <div class="main_app" v-loading="loading">
-    <el-input class="source" v-model="sourceValue" :rows="5" type="textarea" placeholder="请输入需要转换的Json" />
-    <el-button class="commonBtn" type="primary" @click="send">ConvertToCamel</el-button>
-    <el-input class="destination" v-model="destinationValue" :rows="5" type="textarea" placeholder="" />
-    <el-button class="commonBtn" type="primary" @click="copy">ClickToCopy</el-button>
+    <el-input
+      class="source"
+      v-model="sourceValue"
+      :rows="5"
+      type="textarea"
+      placeholder="请输入需要转换的Json"
+    />
+    <el-button class="commonBtn" type="primary" @click="send"
+      >ConvertToCamel</el-button
+    >
+    <el-input
+      class="destination"
+      v-model="destinationValue"
+      :rows="5"
+      type="textarea"
+      placeholder=""
+    />
+    <el-button class="commonBtn" type="primary" @click="copy"
+      >ClickToCopy</el-button
+    >
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
-import { convertToCamel } from '@/api'
-import useClipboard from 'vue-clipboard3'
+import { ref } from "vue";
+import { convertToCamel } from "@/api";
+import useClipboard from "vue-clipboard3";
 
-const { toClipboard } = useClipboard()
+const { toClipboard } = useClipboard();
 
 export default {
-  name: 'app',
+  name: "app",
   data() {
     return {
-      sourceValue: '',
-      destinationValue: '',
+      sourceValue: "",
+      destinationValue: "",
       loading: ref(false),
-    }
+    };
   },
   methods: {
     send() {
-      this.loading = true
+      this.loading = true;
       convertToCamel({
-        jsonStr: this.sourceValue
+        jsonStr: this.sourceValue,
       })
-        .then(res => {
-          console.log(res)
-          this.loading = false
+        .then((res) => {
+          console.log(res);
+          this.loading = false;
           if (res) {
-            this.destinationValue = JSON.stringify(res)
+            this.destinationValue = JSON.stringify(res);
           }
         })
-        .catch(err => {
-          this.loading = false
-          console.log(err)
-        })
+        .catch((err) => {
+          this.loading = false;
+          console.log(err);
+        });
     },
     async copy() {
       try {
-        await toClipboard(this.destinationValue)
-        console.log('Copied to clipboard')
+        await toClipboard(this.destinationValue);
+        console.log("Copied to clipboard");
       } catch (e) {
-        console.error(e)
+        console.error(e);
       }
-    }
+    },
   },
-}
+};
 </script>
 
 <style>
